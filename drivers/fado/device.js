03aadb58b50f40ad0163d9db22a8db0f5b4f43ef
meg768 <magnus@egelberg.se>
2024-12-16 21:03:59 +0100

    -

--- drivers/fado/device.js
+++ drivers/fado/device.js
@@ -22,33 +22,6 @@ class MyDevice extends Device {
 		await this.homey.app.unregisterDevice(this);
 	}
 
-	async addCondition(name, fn) {
-		let condition = this.homey.flow.getConditionCard(name);
-		condition.registerRunListener(fn);
-
-		this.conditions.push(condition);
-	}
-
-	async addAction(name, fn) {
-		let action = this.homey.flow.getActionCard(name);
-		action.registerRunListener(fn);
-
-		this.actions.push(action);
-	}
-
-	async trigger(name, args) {
-		if (args) {
-			this.log(`Triggering '${name}' with parameters ${JSON.stringify(args)}`);
-		} else {
-			this.log(`Triggering '${name}'}`);
-		}
-		const triggerCard = this.homey.flow.getDeviceTriggerCard(name);
-		await triggerCard.trigger(this, args);
-	}
-
-	async onSettings({ oldSettings, newSettings, changedKeys }) {
-		this.log('MyDevice settings where changed');
-	}
 
 
 }
